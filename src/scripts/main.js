c8568418dd1ffb7705a4648a0aedfdb614b2d9c3
Charles Jackson <cpjackson572@gmail.com>
2019-10-18 09:56:15 -0500

    Completed Represent Your Pet Exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 console.log('Focus on YOUR progress compared to YOU...not other people.')
+
+
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+
+const myPet = {
+    name: "Jagger",
+    species: "Doberman Pinscher",
+    nickname: "Kong",
+    Age: 2
+}
+console.log(myPet)
