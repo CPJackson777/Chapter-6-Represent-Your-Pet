fc41b672bdfb1cec0f115c936775cd8dc7ace05c
Charles Jackson <cpjackson572@gmail.com>
2019-10-18 14:59:14 -0500

    Completed the My Pet This portion of the Chapter 6 exercises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,12 +22,28 @@ const myPet = {
     },
     sniffs: function (at) {
         window.alert(`Jagger always sniffs ${at}.`)
+    },
+    favoriteToys: [],
+    play: function (toy) {
+        if (toy.includes('squeaky')) {
+        this.favoriteToys.push(toy);
     }
 }
+}
+    // Regarding lines 27-29: I created a function that uses an if statement to determine a true or false conditional. Therefore, and else statement was not needed. By using the .includes() method, I was able to check for the word squeaky.
 
-    myPet.bark('strangers')
-    myPet.whine('hungry')
-    myPet.sniffs('other dogs')
+    myPet.play ('bone')
+
+    console.log(myPet)
 
 
-console.log(myPet)
+//     myPet.bark('strangers')
+//     myPet.whine('hungry')
+//     myPet.sniffs('other dogs')
+
+
+
+
+// Give your pet a new key named favoriteToys whose value is an empty array.
+// Now define another new key named play whose value is a function with a single parameter named toy.
+// The function should determine if your pet liked the toy, and if s/he does, then it should be added to the favoriteToys array using the this keyword
