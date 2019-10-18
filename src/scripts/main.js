4362db742161ec490af2026ba6a57ae33ee8b3d9
Charles Jackson <cpjackson572@gmail.com>
2019-10-18 14:14:10 -0500

    First attempt at My Pet 'This' exercise. Not sure why this didn't work. Committing this code to research more at a different time.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,12 +22,27 @@ const myPet = {
     },
     sniffs: function (at) {
         window.alert(`Jagger always sniffs ${at}.`)
+    },
+    favoriteToys: [],
+    play: function (toy) {
+        if (`'squeaky' += ${ toy}`) {
+           this.favoriteToys.push(toy); 
+        } 
     }
 }
 
-    myPet.bark('strangers')
-    myPet.whine('hungry')
-    myPet.sniffs('other dogs')
-
-
+myPet.play('bone')
 console.log(myPet)
+
+//     myPet.bark('strangers')
+//     myPet.whine('hungry')
+//     myPet.sniffs('other dogs')
+
+
+// console.log(myPet)
+
+
+// Give your pet a new key named favoriteToys whose value is an empty array.
+// Now define another new key named play whose value is a function with a single parameter named toy.
+// The function should determine if your pet liked the toy, and if s/he does, then it should be added to the favoriteToys array using the this keyword (see example above).
+// Here's an example. If your pet is a cat, then he likely only likes furry toys, so you could put an if..then condition in the play() method that checks if the word "fuzzy" is in the argument value. If it is, it gets added to the array.
